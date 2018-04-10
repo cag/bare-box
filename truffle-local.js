57a2fd11e02287cd5bbcbde1fe819053b6938116
Alan Lu <alanlu1023@gmail.com>
2018-04-10 15:45:41 -0500

    Add a truffle-local.js

---
+++ truffle-local.js
@@ -0,0 +1,5 @@
+// This is a file ignored by source control and NPM which exports
+// a config object merged into the Truffle configuration.
+// Configuration here takes precedence.
+
+module.exports = {}
